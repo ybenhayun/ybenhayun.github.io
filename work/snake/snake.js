e8fe2ee647455cf0470d23c85be8e4dc4ecdabea
Yuval Ben-Hayun <Yuval@Yuvals-MacBook-Pro.local>
2014-12-22 00:10:16 -0500

    fixed bugs

--- work/snake/snake.js
+++ work/snake/snake.js
@@ -5,7 +5,7 @@ var larrow, uarrow, rarrow, darrow;
 var a, d, s, w;
 var canvas, ctx, keystate, frames, score, timer, taken, board;
 var gametype, gamecounter = 0, growth_rate;
-var hx, hy, fx, fy, reset, bomb_reset;
+var hx, hy, fx, fy, nx, ny, reset, bomb_reset;
 
 grid = {
 	width: null, 
@@ -122,13 +122,13 @@ function init() {
 	gamecounter++;
 	reset = false;
 	bomb_reset = false;
-	if (gametype == "portal") set(FRUIT); 
+	if (gametype == "portal") set(FRUIT);
 	set(FRUIT);
 }
 
 function loop() {
-	update();
 	draw();
+	update();
 	
 	globalID = window.requestAnimationFrame(loop, canvas);
 }
@@ -137,84 +137,16 @@ function update() {
 	frames++;
 
 	if ((frames%4 == 0 && gametype == "mover")||(gametype == "disoriented" && frames%8 ==0)){
-		if (reset == true) { 
-			grid.set(SNAKE, fx, fy);
-			reset = false;
-		} else grid.set(EMPTY, fx, fy);
-
-		if (bomb_reset == true) { 
-			grid.set(BOMB, fx, fy);
-			bomb_reset = false;
-		}
-		if (gametype == "mover") fruit_direction = taken;
-		else fruit_direction = Math.floor((Math.random()*15)+1);
-
-		if (fruit_direction%4 == 0) { 
-			fx--;
-			if (fx == grid.width-COLS-1 && gametype == "mover") fx = COLS-1;
-			else if (fx == grid.width-COLS-1) fx++;
-		}
-		else if (fruit_direction%3 == 0) { 
-			fy--;
-			if (fy == grid.height-ROWS-1 && gametype == "mover") fy = ROWS-1;
-			else if (fy == grid.height-ROWS-1) fy++;
-		}
-		else if (fruit_direction%2 == 0) { 
-			fx++;
-			if (fx == COLS && gametype == "mover") fx = grid.width-COLS;
-			else if (fx == COLS) fx--;
-		}
-		else { 
-			fy++;
-			if (fy == ROWS && gametype == "mover") fy = grid.height-ROWS;
-			else if (fy == ROWS) fy--;
-		}
-
-
-
-		if (grid.get(fx, fy) == SNAKE) 
-			reset = true;
-		if (grid.get(fx, fy) == BOMB) bomb_reset = true;
-		if (fx == snake._queue[snake_length-1].x && fy == snake._queue[snake_length-1].y)
-			reset = false;	
-		if (fx == snake._queue[snake_length-2].x && fy == snake._queue[snake_length-2].y)
-			reset = false;
-		if (atHead(fx, fy)) { collectedFruit(fx, fy); reset = false; }
-		grid.set(FRUIT, fx, fy);
+		moveFruit();
 	}		
 
 	if (frames%5 == 0){
-		var nx = snake.last.x;
-		var ny = snake.last.y;
+		nx = snake.last.x;
+		ny = snake.last.y;
 
-		if ((keystate[larrow]||keystate[a]) && snake.direction != right){
-			snake.direction = left;
-		} else if ((keystate[rarrow]||keystate[d]) && snake.direction != left){
-			snake.direction = right;
-		} else if ((keystate[uarrow]||keystate[w]) && snake.direction != down){ 
-			snake.direction = up;
-		} else if ((keystate[darrow]||keystate[s]) && snake.direction != up) {
-			snake.direction = down;
-    	}
-
-		if (snake.direction == left) nx--;
-		else if (snake.direction == up) ny--;
-		else if (snake.direction == right) nx++;
-		else if (snake.direction == down) ny++;
-
-		if (nx < grid.width-COLS){
-			if (grid.get(COLS-1, ny) == SNAKE) return init();
-			nx = COLS-1;
-		} else if (nx > COLS-1) {
-			if (grid.get(grid.width-COLS, ny) == SNAKE) return init();
-			nx = grid.width-COLS;
-		} else if (ny < grid.height-ROWS) {
-			if (grid.get(nx, ROWS-1) == SNAKE) return init();
-			ny = ROWS-1;
-		} else if (ny > ROWS-1) {
-			if (grid.get(nx, grid.height-ROWS) == SNAKE) return init();
-			ny = grid.height-ROWS;
-		} else if (gameOver(nx, ny)) {
+		moveSnake();
+		
+		if (gameOver(nx, ny)) {
 			reset = false;
 			return init();
 		}
@@ -265,9 +197,10 @@ function collectedFruit(x, y){document.getElementById("fruitsound").play();
 		for (var i = 0; i < COLS; i++){
 			for(var j = 0; j < ROWS; j++){
 				if (grid.get(i, j) == FRUIT){
+					grid.set(SNAKE, i, j);
 					snake.last.x = i;
 					snake.last.y = j;
-					grid.set(SNAKE, i, j);
+					hx = i; hy = j;
 					break;
 				}
 			}
@@ -350,6 +283,78 @@ function draw() {
 	document.getElementById("score").innerHTML += "<br><br>HIGH SCORE: " + localStorage.getItem(gametype);
 	document.getElementById("score").innerHTML += "<br>MOST FRUIT: " + localStorage.getItem(gametype+'fruit');
 
+}
+
+function moveSnake(){
+	if ((keystate[larrow]||keystate[a]) && snake.direction != right){
+				snake.direction = left;
+	} else if ((keystate[rarrow]||keystate[d]) && snake.direction != left){
+		snake.direction = right;
+	} else if ((keystate[uarrow]||keystate[w]) && snake.direction != down){ 
+		snake.direction = up;
+	} else if ((keystate[darrow]||keystate[s]) && snake.direction != up) {
+		snake.direction = down;
+   	}
+	if (snake.direction == left) nx--;
+	else if (snake.direction == up) ny--;
+	else if (snake.direction == right) nx++;
+	else if (snake.direction == down) ny++;
+
+	if (nx < grid.width-COLS){
+		if (grid.get(COLS-1, ny) == SNAKE) return init();
+		nx = COLS-1;
+	} else if (nx > COLS-1) {
+		if (grid.get(grid.width-COLS, ny) == SNAKE) return init();
+		nx = grid.width-COLS;
+	} else if (ny < grid.height-ROWS) {
+		if (grid.get(nx, ROWS-1) == SNAKE) return init();
+		ny = ROWS-1;
+	} else if (ny > ROWS-1) {
+		if (grid.get(nx, grid.height-ROWS) == SNAKE) return init();
+		ny = grid.height-ROWS;
+	}
+}
+
+function moveFruit(){
+	if (reset == true) { 
+		grid.set(SNAKE, fx, fy);
+		reset = false;
+	} else grid.set(EMPTY, fx, fy);
+
+	if (bomb_reset == true) { 
+		grid.set(BOMB, fx, fy);
+		bomb_reset = false;
+	}
+	if (gametype == "mover") fruit_direction = taken;
+	else fruit_direction = Math.floor((Math.random()*15)+1);
+
+	if (fruit_direction%4 == 0) { 
+		fx--;
+		if (fx == grid.width-COLS-1 && gametype == "mover") fx = COLS-1;
+		else if (fx == grid.width-COLS-1) fx++;
+	} else if (fruit_direction%3 == 0) { 
+		fy--;
+		if (fy == grid.height-ROWS-1 && gametype == "mover") fy = ROWS-1;
+		else if (fy == grid.height-ROWS-1) fy++;
+	} else if (fruit_direction%2 == 0) { 
+		fx++;
+		if (fx == COLS && gametype == "mover") fx = grid.width-COLS;
+		else if (fx == COLS) fx--;
+	} else { 
+		fy++;
+		if (fy == ROWS && gametype == "mover") fy = grid.height-ROWS;
+		else if (fy == ROWS) fy--;
+	}
+
+	if (grid.get(fx, fy) == SNAKE) 
+		reset = true;
+	if (grid.get(fx, fy) == BOMB) bomb_reset = true;
+	if (fx == snake._queue[snake_length-1].x && fy == snake._queue[snake_length-1].y)
+		reset = false;	
+	if (fx == snake._queue[snake_length-2].x && fy == snake._queue[snake_length-2].y)
+		reset = false;
+	if (atHead(fx, fy)) { collectedFruit(fx, fy); reset = false; }
+	grid.set(FRUIT, fx, fy);
 }
 
 function setGame(game){
