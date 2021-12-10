9de5eac6a741012877760d11f4649abc914a2883
BEN HAYUN <ybenhayun@gmail.com>
2021-12-09 23:06:50 -0500

    pushing

--- work/snake/snake.js
+++ work/snake/snake.js
@@ -264,7 +264,6 @@ function collectedFruit(x, y){
 	if (taken > localStorage.getItem(gametype+location.pathname+'fruit'))
 		localStorage.setItem(gametype+location.pathname+'fruit', taken);
 
-	////alert(location.pathname); //twice
 
 	if (gametype == "portal") set(FRUIT);
 	set(FRUIT);
