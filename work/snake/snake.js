1a74e64fac14c490edaf487c7e43b80022f7e943
BEN HAYUN <ybenhayun@gmail.com>
2021-12-11 13:31:02 -0500

    adding

--- work/snake/snake.js
+++ work/snake/snake.js
@@ -264,7 +264,7 @@ function collectedFruit(x, y){
 	if (taken > localStorage.getItem(gametype+location.pathname+'fruit'))
 		localStorage.setItem(gametype+location.pathname+'fruit', taken);
 
-	//this works now? it doesnt. one last time then im going to sleep
+	//this works now? it doesnt. one last time then im going to sleep sleep again!
 	if (gametype == "portal") set(FRUIT);
 	set(FRUIT);
 
