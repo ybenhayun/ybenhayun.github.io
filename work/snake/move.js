0fe2c6e1bfc591dc7846e36871cb87b94a276c94
Yuval Ben-Hayun <ybenhayun@gmail.com>
2022-04-06 13:21:29 -0400

    Delete work directory

--- work/snake/move.js
+++
@@ -1,9 +0,0 @@
-function moveSnake(){
-	if ((keystate[larrow]) && snake[0].direction != right) snake[0].direction = left;
-	else if ((keystate[rarrow]) && snake[0].direction != left) snake[0].direction = right;
-	else if ((keystate[uarrow]) && snake[0].direction != down) snake[0].direction = up;
-	else if ((keystate[darrow]) && snake[0].direction != up) snake[0].direction = down;
-
-	snake[0].head.x = newPosition(SNAKE, snake[0].direction, snake[0].head.x, snake[0].head.y).x;
-	snake[0].head.y = newPosition(SNAKE, snake[0].direction, snake[0].head.x, snake[0].head.y).y;
-}
